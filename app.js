dd46ef321f323b4f47e1435750427bb0266cc0f4
Edwin Mancilla <edwinhuevo10@gmail.com>
2025-02-03 20:59:48 -0600

    amigo secreto primer paso

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el
+let nombreAmigos = [];
+
+
+function agregarAmigo() {
+    let amigos = document.getElementById ('amigo').value;
+    limpiarCaja();
+    console.log(amigos);
+  if(amigos == ''){
+    alert('ingrese un nombre valido');
+  } else if(!nombreAmigos.includes(amigos)){
+    nombreAmigos.push(amigos);
+    return amigos;
+  }
+
+}
+
+function limpiarCaja(){
+    document.getElementById('amigo').value= '';
+}
+console.log(nombreAmigos);
+
+let lista = document.getElementById('listaAmigos');
+lista.innerHTML='';
